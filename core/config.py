9c24f162950b6d47144337e9ef8ca5b4ad757eb5
AjitTiwari85 <at856575@gmail.com>
2026-02-07 16:30:15 +0530

    Add damage threshold and fraud detection config

---
+++ core/config.py
@@ -0,0 +1,16 @@
+DAMAGE_THRESHOLD = 25000
+
+FRAUD_KEYWORDS = [
+    "fraud",
+    "staged",
+    "inconsistent"
+]
+
+MANDATORY_FIELDS = [
+    "policyNumber",
+    "policyholderName",
+    "incidentDate",
+    "claimType",
+    "estimatedDamage",
+    "initialEstimate"
+]
